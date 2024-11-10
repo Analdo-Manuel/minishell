ec51c5d817c8a6bfe0cedd70044287715e7f8001
Analdo Manuel <analdomanuel18@gmail.com>
2024-11-10 01:00:05 +0100

    alteracao no unset

--- srcs/builtins/binarios_two.c
+++ srcs/builtins/binarios_two.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   binarios_two.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almanuel <almanuel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: analdo <analdo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:36:44 by almanuel          #+#    #+#             */
-/*   Updated: 2024/11/08 16:42:11 by almanuel         ###   ########.fr       */
+/*   Updated: 2024/11/10 00:55:50 by analdo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,25 @@ static
 		int	ft_strcmp_unset_export(const char *s1, const char *s2)
 {
 	size_t	i;
+	size_t	j;
 	size_t	k;
 
 	i = 0;
+	j = 0;
 	k = -1;
+	while (s2[j] && s2[j] != 'x')
+		j++;
+	j += 2;
 	while (s1[++k] && s1[k] != '=')
 		;
-	while (s1[i] && s2[i] && (s1[i] == s2[i]))
+	while (s1[i] && s2[j] && (s1[i] == s2[j]))
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] == s2[j])
 			k--;
 		i++;
+		j++;
 	}
-	if (s2[i] == '=' && k == 0)
+	if (s2[j] == '=' || k == 0)
 		return (k);
 	return (1);
 }
@@ -64,17 +70,35 @@ char	**builtins_unset(char **envp, char *clear, t_valuer val)
 	val.p = (char **)malloc(sizeof(char *) * (val.i + 1));
 	while (envp[val.j])
 	{
-		if (ft_strcmp_unset(clear, envp[val.j]) != 0)
+		if (val.signal == true)
 		{
-			val.i = -1;
-			while (envp[val.j][++val.i])
-				;
-			val.p[val.k] = (char *)malloc(sizeof(char) * (val.i + 1));
-			val.i = -1;
-			while (envp[val.j][++val.i])
-				val.p[val.k][val.i] = envp[val.j][val.i];
-			val.p[val.k][val.i] = '\0';
-			val.k++;
+			if (ft_strcmp_unset(clear, envp[val.j]) != 0)
+			{
+				val.i = -1;
+				while (envp[val.j][++val.i])
+					;
+				val.p[val.k] = (char *)malloc(sizeof(char) * (val.i + 1));
+				val.i = -1;
+				while (envp[val.j][++val.i])
+					val.p[val.k][val.i] = envp[val.j][val.i];
+				val.p[val.k][val.i] = '\0';
+				val.k++;
+			}
+		}
+		else
+		{
+			if (ft_strcmp_unset_export(clear, envp[val.j]) != 0)
+			{
+				val.i = -1;
+				while (envp[val.j][++val.i])
+					;
+				val.p[val.k] = (char *)malloc(sizeof(char) * (val.i + 1));
+				val.i = -1;
+				while (envp[val.j][++val.i])
+					val.p[val.k][val.i] = envp[val.j][val.i];
+				val.p[val.k][val.i] = '\0';
+				val.k++;
+			}
 		}
 		val.j++;
 	}
