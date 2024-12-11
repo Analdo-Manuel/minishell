95f0836a02f4050ae555be1382051e87822ec8be
Marco Carvalho <marccarv@c1r8s1.42luanda.com>
2024-12-11 12:38:38 +0100

    pipe feitos

--- srcs/ft_split_pipe.c
+++ srcs/ft_split_pipe.c
@@ -3,25 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_pipe.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almanuel <almanuel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marccarv <marccarv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:21:18 by marccarv          #+#    #+#             */
-/*   Updated: 2024/12/10 12:14:07 by almanuel         ###   ########.fr       */
+/*   Updated: 2024/12/11 12:36:50 by marccarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	ft_wordcount(const char *str, char c)
+static int	ft_word_matrix(const char *str, char c)
 {
 	int	i;
 	int	count;
+	int	m = 0;
+	int	n = 0;
 
 	i = 0;
+	m = 0;
+	n = 0;
 	count = 0;
 	while (str[i])
 	{
-		if (str[i] != c && (i == 0 || str[i - 1] == c))
+		if (str[i] == 34 && m % 2 == 0)
+			n++;
+		else if (str[i] == 39 && n % 2 == 0)
+			m++;
+		if (str[i] == c && m % 2 == 0 && n % 2 == 0) // analisar melhor
 			count++;
 		i++;
 	}
@@ -31,29 +39,49 @@ static int	ft_wordcount(const char *str, char c)
 static int	ft_wordcpy(const char *str, char c, int i)
 {
 	int	j;
+	int	m = 0;
+	int	n = 0;
 
 	j = 0;
-	while (str[i + j] && str[i + j] != c)
+	while (str[i + j])
+	{
+		if (str[i + j] == 34 && m % 2 == 0)
+			n++;
+		else if (str[i + j] == 39 && n % 2 == 0)
+			m++;
+		if (str[i + j] == c && m % 2 == 0 && n % 2 == 0)
+			break ;
 		j++;
+	}
 	return (j);
 }
-
+// Tinha um erro aqui no ft_wordcpy, estava retornando j - 1 porque começa com zero, o que fazia com que o último caractere não fosse copiado
 char	**ft_split_pipe(char const *str, char c)
 {
 	char	**p;
 	int		i;
 	int		j;
 	int		k;
+	int		m = 0;
+	int		n = 0;
 
-	p = (char **) malloc(sizeof(char *) * (ft_wordcount(str, c) + 1));
 	i = 0;
 	j = 0;
+	p = (char **) malloc(sizeof(char *) * (ft_word_matrix(str, c) + 2));
 	while (str[i])
 	{
 		p[j] = (char *) malloc(sizeof(char) * (ft_wordcpy(str, c, i) + 2));
 		k = 0;
-		while (str[i] && str[i] != c)
+		while (str[i])
+		{
+			if (str[i] == 34 && m % 2 == 0)
+				n++;
+			else if (str[i] == 39 && n % 2 == 0)
+				m++;
+			if (str[i] == c && m % 2 == 0 && n % 2 == 0)
+				break ;
 			p[j][k++] = str[i++];
+		}
 		if (str[i] == c)
 		{
 			i++;
--- srcs/verefiy_pipe.c
+++ srcs/verefiy_pipe.c
@@ -6,7 +6,7 @@
 /*   By: marccarv <marccarv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 12:53:18 by marccarv          #+#    #+#             */
-/*   Updated: 2024/11/29 16:18:32 by marccarv         ###   ########.fr       */
+/*   Updated: 2024/12/11 12:37:01 by marccarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	verefiy_pipe(char *str)
 	int	i;
 	int	j = 0;
 	int	l = 0;
+	int m = 0;
     int pipe = 0;
 
 	i = 0;
@@ -26,7 +27,23 @@ int	verefiy_pipe(char *str)
 			j++;
 		else if (str[i] == 39 && j % 2 == 0)
 			l++;
-		if (str[i] == 124 && str[i + 1] == 124 \
+		if (str[i] == 124 && str[i - 1] == '\0' \
+		&& j % 2 == 0 && l % 2 == 0)
+		{
+			while (str[i] == 124)
+				i++;
+			printf("bash: syntax error near unexpected token `%c'\n", str[i - 1]);
+			return (3);
+		}
+		else if (str[i] == 124 && str[i + 1] == 124 \
+		&& j % 2 == 0 && l % 2 == 0)
+		{
+			while (str[i] == 124)
+				i++;
+			printf("bash: syntax error near unexpected token `%c'\n", str[i - 1]);
+			return (3);
+		}
+		else if (str[i] == 124 && str[i + 1] == 124 \
 		&& str[i + 2] == 124 && j % 2 == 0 && l % 2 == 0)
 		{
 			while (str[i] == 124)
@@ -34,11 +51,22 @@ int	verefiy_pipe(char *str)
 			printf("bash: syntax error near unexpected token `%c'\n", str[i - 1]);
 			return (3);
 		}
-		else if (str[i] == 124 && str[i + 1] != 124 \
-		&& str[i -1] != 124 && j % 2 == 0 && l % 2 == 0)
+		else if (str[i] == 124 \
+		&& j % 2 == 0 && l % 2 == 0)
 		{
-			pipe++;
+			m = i;
+			m++;
+			while (str[m] && (str[m] == 32 || str[m] == 9))
+				m++;
+			if (str[m] == '\0' || str[m] == 124)
+			{
+				printf("bash: syntax error near unexpected token `|'\n");
+				return (3);
+			}
 		}
+		if (str[i] == 124 && str[i + 1] != 124 \
+		&& str[i -1] != 124 && j % 2 == 0 && l % 2 == 0)
+			pipe++;
 		i++;
 	}
 	if (pipe > 0)
